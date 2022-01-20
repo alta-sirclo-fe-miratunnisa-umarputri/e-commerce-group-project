35ed6ffab2e957cbd047638f5c957eae00fcf419
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-01-20 14:10:38 +0700

    feat: add more skeleton page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Button from "./components/Button";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import MyProduct from "./pages/MyProduct";
 import Order from "./pages/Order";
@@ -16,6 +17,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/order" element={<Order />} />
         <Route path="/myproduct" element={<MyProduct />} />
         <Route path="/profile" element={<Profile />} />
