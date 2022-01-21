342682bd5dd5487a0f4a0e81a0dc4f0bb7177982
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-01-21 21:46:12 +0700

    feat: create LoginModal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 
-import Button from "./components/Button";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import LoginModal from "./components/LoginModal";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import MyProduct from "./pages/MyProduct";
@@ -17,6 +17,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginModal />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/order" element={<Order />} />
         <Route path="/myproduct" element={<MyProduct />} />
---
+++ src/components/LoginModal.tsx
@@ -0,0 +1,53 @@
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+
+const FormModal = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate("/");
+  };
+
+  const handleClose = () => {
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <Dialog open={true} onClose={handleClose}>
+        <DialogTitle>Login</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="email"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="password"
+            label="Password"
+            type="password"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleLogin}>Login</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default FormModal;
