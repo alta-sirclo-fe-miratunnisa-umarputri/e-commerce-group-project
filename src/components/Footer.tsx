c2d8af6b42ead79f9e9ba85dee3fd8b586d8ccd5
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-01-21 15:14:55 +0700

    feat: create Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,69 @@
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+
+import style from "./Footer.module.css";
+import logo from "../assets/logo.png";
+import visaMaster from "../assets/visa-master.png";
+import gooAppStore from "../assets/goo-app-store.png";
+
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <footer className={style.position}>
+      <Box py={{ xs: 1, sm: 1 }} bgcolor="text.secondary">
+        <Container maxWidth="lg">
+          <Grid container>
+            <Grid
+              item
+              xs={12}
+              sm={4}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <img src={logo} alt="logo-footer" className={style.logoSirclo} />
+            </Grid>
+
+            <Grid
+              item
+              xs={12}
+              sm={4}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <img
+                src={visaMaster}
+                alt="visa-master"
+                className={style.logoVisaMaster}
+              />
+            </Grid>
+
+            <Grid
+              item
+              xs={12}
+              sm={4}
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <img
+                src={gooAppStore}
+                alt="google-app-store"
+                className={style.logoGooApp}
+              />
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </footer>
+  );
 };
 
 export default Footer;
