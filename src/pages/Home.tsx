5ad3ce9c02a4f4f72475407ee44bc23804019898
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-01-23 17:26:17 +0700

    feat: create Home page component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,71 @@
-import { Fragment } from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+  Button,
+  Container,
+} from "@mui/material";
+import { useState } from "react";
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const Home = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  const handleDetail = () => {
+    console.log("detail");
+  };
+
+  const handleAddToCart = () => {
+    console.log("add to cart");
+  };
+
   return (
-    <Fragment>
-      <h1>Home</h1>
-    </Fragment>
+    <Container>
+      <Typography variant="h3" gutterBottom mt={3} textAlign="center">
+        All Products
+      </Typography>
+
+      <Grid container spacing={3}>
+        {cards.map((card) => (
+          <Grid item key={card} xs={12} sm={6} md={3}>
+            <Card
+              sx={{ height: "100%", display: "flex", flexDirection: "column" }}
+            >
+              <CardMedia
+                component="img"
+                image="https://source.unsplash.com/random"
+                alt="random"
+              />
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography
+                  // gutterBottom
+                  variant="h6"
+                  component="h3"
+                  fontWeight={400}
+                >
+                  Item's Name
+                </Typography>
+                <Typography variant="h5" component="h2" fontWeight={500}>
+                  Rp xxxx
+                </Typography>
+              </CardContent>
+              <CardActions sx={{ display: "flex", justifyContent: "end" }}>
+                <Button size="small" onClick={handleDetail}>
+                  Detail
+                </Button>
+                <Button size="small" onClick={handleAddToCart}>
+                  Add to Cart
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
