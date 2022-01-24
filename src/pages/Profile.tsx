df656dc2b06902740bebacf87bba80f08d844e3d
fermidrates <albertfarand@gmail.com>
2022-01-24 17:07:06 +0700

    fix: resolve conflict to merge profile page and add item modal page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,109 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import ProfileImage from '../assets/app-store.png';
+import './Profile.css';
+import axios from 'axios';
+
 const Profile = () => {
-  return <h1>Profile</h1>;
+  const navigate = useNavigate();
+  const URL = "http://3.0.145.2/users";
+  const accessToken = localStorage.getItem("accessToken");
+  const [profile, setProfile] = useState<any[]>([]);
+  
+  React.useEffect(() => {
+    fetchData()
+  }, []);
+
+  const fetchData = () => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      },
+    };
+    axios
+      .get(URL, config)
+      .then((res) => {
+        const profileExtract = res.data.data;
+        setProfile(profileExtract);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(
+      )
+  }
+
+  const handleEditProfile = () => {
+    navigate("/editprofile");
+  }
+
+  const handleEditPassword = () => {
+    navigate("/editpassword");
+  }
+
+  const handleDeleteAccount = () => {
+    navigate("/");
+    // delete account
+  }
+
+  return (
+    <div className='main-container'>
+      <div className='photo-container'>
+        <img
+          src={ ProfileImage } width="200px" height="200px" alt=""/>
+      </div>
+      <div className='table-container'>
+        <table>
+          <tbody>
+            <tr>
+              <td>Username</td>
+            </tr>
+            <tr>
+              <td>{profile[0]}</td>
+            </tr>
+            <tr>
+              <td>E-mail</td>
+            </tr>
+            <tr>
+              <td>{profile[1]}</td>
+            </tr>
+            <tr>
+              <td>Handphone Number</td>
+            </tr>
+            <tr>
+              <td>{profile[2]}</td>
+            </tr>
+            <tr>
+              <td>Address</td>
+            </tr>
+            <tr>
+              <td>{profile[5]}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className='button-container'>
+        <Button 
+          className='button1'
+          onClick={() => handleEditProfile()}
+          variant="outlined" size="small">Edit Profile
+          </Button> <br />
+        <Button 
+          className='button2'
+          onClick={() => handleEditPassword()}
+          variant="outlined" size="small">Change Password
+          </Button> <br />
+        <Button
+          className='button3' 
+          onClick={() => handleDeleteAccount()}
+          variant="outlined" size="small" color="error">Delete Account
+          </Button>
+      </div>
+    </div>
+  )
 };
 
 export default Profile;
